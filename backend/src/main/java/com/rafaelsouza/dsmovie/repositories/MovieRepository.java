8ef439e9613a3e65f5ee4351073fb97ecae01746
Rafael Souza <rafael.gustavo221188@gmail.com>
2022-05-26 00:06:20 -0300

    Find movies

---
+++ backend/src/main/java/com/rafaelsouza/dsmovie/repositories/MovieRepository.java
@@ -0,0 +1,11 @@
+package com.rafaelsouza.dsmovie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rafaelsouza.dsmovie.entities.Movie;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+	
+	
+
+}
