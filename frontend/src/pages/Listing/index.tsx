18847c05f912aea33a78186af31ec31c76421afb
Rafael Souza <rafael.gustavo221188@gmail.com>
2022-06-21 17:00:12 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,15 +1,37 @@
-import axios from "axios";
-import MovieCard from "components/MovieCard";
-import Pagination from "components/Pagination";
-import { BASE_URL } from "utils/requests";
+import axios from 'axios';
+import MovieCard from 'components/MovieCard';
+import Pagination from 'components/Pagination';
+import { useEffect, useState } from 'react';
+import { MoviePage } from 'types/movie';
+import { BASE_URL } from 'utils/requests';
 
 function Listing() {
-
-    // FORMA ERRADA
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
-    .then(response => {
-        console.log(response.data);
+    const [pageNumber, setPageNumber] = useState(0);
+  
+    const [page, setPage] = useState<MoviePage>({
+      content: [],
+      last: true,
+      totalPages: 0,
+      totalElements: 0,
+      size: 12,
+      number: 0,
+      first: true,
+      numberOfElements: 0,
+      empty: true
     });
+  
+    useEffect(() => {
+      axios.get(`${ BASE_URL }/movies?size=12&page=${ pageNumber }`)
+        .then(response => {
+          const data = response.data as MoviePage;
+          setPage(data);
+        });
+    }, [pageNumber]);
+  
+    const handlePageChange = (newPageNumber: number) => {
+      setPageNumber(newPageNumber);
+    }
+  
 
     return (
         <>
